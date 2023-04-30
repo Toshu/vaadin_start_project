2de4a3706d05b85b0dff1ae597b92ffb419ef4df
Toshu Amashu <toshude@yahoo.de>
2023-04-30 16:42:44 +0200

    first (empty) view with table

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -16,7 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Theme(value = "flowcrmtutorial")
 public class Application implements AppShellConfigurator {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
